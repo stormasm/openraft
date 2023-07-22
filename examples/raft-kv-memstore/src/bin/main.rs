98402ef3c308e9820a5746e20822958ba5b66fda
Michael Angerman <1809991+stormasm@users.noreply.github.com>
2023-07-22 10:47:47 -0700

    completely remove the code formatting the tracing_subscriber as it is available in the main branch

--- examples/raft-kv-memstore/src/bin/main.rs
+++ examples/raft-kv-memstore/src/bin/main.rs
@@ -1,6 +1,5 @@
 use clap::Parser;
 use raft_kv_memstore::start_example_raft_node;
-use tracing_subscriber::EnvFilter;
 
 #[derive(Parser, Clone, Debug)]
 #[clap(author, version, about, long_about = None)]
@@ -15,16 +14,6 @@ pub struct Opt {
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
     console_subscriber::init();
-    // Setup the logger
-    /*
-    tracing_subscriber::fmt()
-        .with_target(true)
-        .with_thread_ids(true)
-        .with_level(true)
-        .with_ansi(false)
-        .with_env_filter(EnvFilter::from_default_env())
-        .init();
-        */
     // Parse the parameters passed by arguments.
     let options = Opt::parse();
 
